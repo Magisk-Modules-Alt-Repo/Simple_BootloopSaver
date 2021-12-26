82251dfe7d266b580ae57b4d70a4c5412dcc1280
HuskyDG <84650617+HuskyDG@users.noreply.github.com>
2021-12-26 18:17:52 +0700

    detect zygote not start
    fix modules not work in that case

--- service.sh
+++ service.sh
@@ -1,5 +1,74 @@
 #!/system/bin/sh
 #Bootloop saver by HuskyDG
+
 MODDIR=${0%/*}
-chmod 750 "$MODDIR/saver.exec"
-$MODDIR/saver.exec process
+FILE="$0"; NAME="$1"; VALUE="$2"; MODID="/data/adb/modules/huskydg_bootloopsaver"; VX="$@"
+abort(){ echo "$1"; exit 1; }
+
+# based on canyie dreamland framework script
+
+
+write_log(){ 
+TEXT=$@; echo "[`date +%d%m%y` `date +%T`]: $TEXT" >>/cache/bootloop_saver.log 
+}
+
+exit_log(){
+write_log "$@"; exit 0;
+}
+
+disable_modules(){
+write_log "zygote meets the trouble, disable all modules and restart"
+
+list="$(find /data/adb/modules/* -prune -type d)"
+IFS=$"
+"
+for module in $list; do
+echo -n >> $module/disable
+done
+reboot
+exit
+}
+
+
+
+
+mv -rf /cache/bootloop_saver.log /cache/bootloop_saver.log.bak 2>/dev/null
+write_log "bootloop saver started"
+MAIN_ZYGOTE_NICENAME=zygote
+CPU_ABI=$(getprop ro.product.cpu.api)
+[ "$CPU_ABI" = "arm64-v8a" -o "$CPU_ABI" = "x86_64" ] && MAIN_ZYGOTE_NICENAME=zygote64
+
+# Wait for zygote starts
+sleep 5
+
+ZYGOTE_PID1=$(pidof "$MAIN_ZYGOTE_NICENAME" | awk '{ print $1 '})
+write_log "pid of zygote stage 1: $ZYGOTE_PID1"
+sleep 15
+ZYGOTE_PID2=$(pidof "$MAIN_ZYGOTE_NICENAME" | awk '{ print $1 '})
+write_log "pid of zygote stage 2: $ZYGOTE_PID2"
+sleep 15
+ZYGOTE_PID3=$(pidof "$MAIN_ZYGOTE_NICENAME" | awk '{ print $1 '})
+write_log "pid of zygote stage 3: $ZYGOTE_PID3"
+
+
+if [ "$ZYGOTE_PID1" = "$ZYGOTE_PID2" ] && [ "$ZYGOTE_PID2" = "$ZYGOTE_PID3" ]; then
+    if [ -z "$ZYGOTE_PID1" ]; then
+        write_log "maybe zygote not start :("
+        disable_modules
+    else
+        exit_log "pid of 3 stage zygote is the same"
+    fi
+else
+    write_log "pid of 3 stage zygote is different, continue check to make sure... "
+fi
+
+
+
+
+sleep 15
+ZYGOTE_PID4=$(pidof "$MAIN_ZYGOTE_NICENAME" | awk '{ print $1 '})
+write_log "pid of zygote stage 4: $ZYGOTE_PID4"
+[ "$ZYGOTE_PID3" = "$ZYGOTE_PID4" ] && exit_log "pid of zygote stage 3 and 4 is the same."
+
+disable_modules
+
